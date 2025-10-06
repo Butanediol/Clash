8c7d0e69289411daaec9dbae40cdfa029978c607
Butanediol <git@xnh.app>
2025-10-06 22:36:58 +0800

    feat(app): implement single instance application logic using Mutex

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Threading;
 using System.Windows;
 
 namespace ClashXW
@@ -7,5 +9,27 @@ namespace ClashXW
     /// </summary>
     public partial class App : Application
     {
+        private static Mutex? _mutex;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            const string mutexName = "ClashXW_SingleInstance_Mutex";
+            _mutex = new Mutex(true, mutexName, out bool createdNew);
+
+            if (!createdNew)
+            {
+                Shutdown();
+                return;
+            }
+
+            base.OnStartup(e);
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            _mutex?.ReleaseMutex();
+            _mutex?.Dispose();
+            base.OnExit(e);
+        }
     }
 }
